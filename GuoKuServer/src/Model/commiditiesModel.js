5a08172ed4dc35122e1ce5eaa765994d5a05ea82
宋熙明 <18701593595@163.com>
2016-01-22 20:04:30 +0800

    fix(server): modify commidyty

--- GuoKuServer/src/Model/commiditiesModel.js
+++ GuoKuServer/src/Model/commiditiesModel.js
@@ -1,19 +1,19 @@
 var mongoose = require('mongoose'); 
 var CommiditySchema = new mongoose.Schema({
 	  title: {type: String },
-	  content: {type: String },
 	  price: {type: String },
 	  date: {type: String },
 	  comment: {type: [String]},
 	  commentAuthor: {type: [String]},
 	  commentImg: {type: [String]},
-    imgPath1: {type: String },
-  	imgPath2: {type: String },
-  	imgPath3: {type: String },
-  	category: {type: String},
+	  comemntDate: {type: [String]},
+      imgPath1: {type: String },
+  	  imgPath2: {type: String },
+  	  imgPath3: {type: String },
+  	  category: {type: String},
 	  categoryImg: {type: String},
-  	love: {type: Number},
-  	loveHeadImg: {type: [String]}
+  	  love: {type: Number},
+  	  loveHeadImg: {type: [String]}
 });
 var CommidityModel = mongoose.model('commidities', CommiditySchema);
 module.exports = CommidityModel;
--- GuoKuServer/src/search.js
+++ GuoKuServer/src/search.js
@@ -1,7 +1,7 @@
 var Commidity = require("./Model/commiditiesModel");
 function search(response,request,key){
 	var search = new RegExp(key);
-  var query = [{ "title": search},{'content':search}]
+  var query = [{ "title": search},{'comment':search},{'category':search}]
   console.log(query);
   Commidity.find({"$or":query},function(err, articles) {
     if (err) return console.error(err);
