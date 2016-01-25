91c5b24a8a04fde21228a2de3309cc14d86ee697
宋熙明 <18701593595@163.com>
2016-01-25 19:27:00 +0800

    fix: modify graphic

--- GuoKuServer/src/GraphicModel.js
+++ GuoKuServer/src/GraphicModel.js
@@ -1,12 +1,10 @@
 var mongoose = require('mongoose'); 
 var GraphicSchema = new mongoose.Schema({
 	  title: {type: String },
-	  content: {type: String },
-	  author: {type: String },
+	  describe: {type: String },
+	  url: {type: String },
 	  date: {type: String },
-	  comment: {type: [String]},
-	  commentAuthor: {type: [String]},
-      imgPath: {type: String }
+	  imgPath: {type: String},
 });
 var GraphicModel = mongoose.model('graphics', GraphicSchema);
 module.exports = GraphicModel;
