1059881cda6bd489ddc0a6069802b96162577176
宋熙明 <18701593595@163.com>
2016-01-25 10:38:47 +0800

    fix: ios

--- GuoKuApp/index.ios.js
+++ GuoKuApp/index.ios.js
@@ -9,5 +9,7 @@ var RegisterScreen = require('./src/screens/UserLRScreen/RegisterScreen')
 var SetUpScreen = require('./src/screens/SettingScreen')
 var SearchScreen = require('./src/screens/SearchScreen')
 var MainRoute = require('./src/screens/MainRoute')
+var CommodityScreen = require('./src/screens/CommodityScreen')
+var UserDetailScreen = require('./src/screens/UserLRScreen/UserDetailScreen')
 
-React.AppRegistry.registerComponent('GuoKuApp', () => SearchScreen)
+React.AppRegistry.registerComponent('GuoKuApp', () => GuoKuRunner)
